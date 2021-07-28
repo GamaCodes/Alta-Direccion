a17451b594f7034fc51c5418124828928bb20df6
ASdeveloper-app <arturo.araujo.alvarez@gmail.com>
2021-07-28 00:27:18 -0500

    loading

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from "./config.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="App-link">PROXIMAMENTE</p>
         <p>Sitio en construcción</p>
+        <img src="consul_logotipo.svg" className="App-logo" alt="logo" />
+        <p className="App-link">Consultores En</p>
+        <p className="App-red">Alta Dirección</p>
       </header>
     </div>
   );
